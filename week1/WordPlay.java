9beb4f02d3de30c0aa77f135f2c291578c894525
acer42 <qduong@students.42wolfsburg.de>
2023-12-10 15:23:32 +0100

    emphasize + replace vowels

--- week1/WordPlay.java
+++ week1/WordPlay.java
@@ -14,13 +14,31 @@ public class WordPlay{
 		}
 		return sb.toString();
 	}
+	private static String emphasize(String phrase, char ch){
+		char c = Character.toLowerCase(ch);
+		String str = phrase.toLowerCase();
+		StringBuilder sb = new StringBuilder(phrase);
+		for (int i = 0; i < str.length(); i++) {
+				if(str.charAt(i) == c)
+				{
+					if (i % 2 == 0)
+						sb.setCharAt(i, '*');
+					else
+						sb.setCharAt(i, '+');
+				}
+		}
+		return sb.toString();
+	}
 	public static void main(String[]args){
 		//test 1
 		//boolean bisVowel = isVowel('a');
 		// System.out.println(bisVowel);
 		//test2
-		String phrase = "Hello World";
-		String result = replaceVowels(phrase, '*');
-		System.out.println("result: " + result);
+		// String phrase = "Hello World";
+		// String result = replaceVowels(phrase, '*');
+		// System.out.println("result: " + result);
+		//test3
+		System.out.println(emphasize("dna ctgaaactga", 'a'));
+		System.out.println(emphasize("Mary Bella Abracadabra",'a'));
 	}
 }
