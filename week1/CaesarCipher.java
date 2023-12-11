453b754f5c70d7cedc14e0ef101ffe24e0c40ad5
acer42 <qduong@students.42wolfsburg.de>
2023-12-11 16:55:22 +0100

    encrypt two keys

--- week1/CaesarCipher.java
+++ week1/CaesarCipher.java
@@ -21,18 +21,63 @@ public class CaesarCipher{
 		}
 		return outputsb.toString();
 	}
+	private static String encryptTwoKeys(String input, int key1, int key2){
+		if (key1 > 25)
+			key1 = key1 % 26;
+		if (key2 > 25)
+			key2 = key2 % 26;
+		String lowerInput = input.toLowerCase();
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		String Cipher1 = alphabet.substring(key1) + alphabet.substring(0,key1);
+		String Cipher2 = alphabet.substring(key2) + alphabet.substring(0,key2);
+		StringBuilder outputsb = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) {
+			if(outputsb.length() != i)
+				outputsb.append(input.charAt(i - 1));
+			if (i % 2 == 0){
+				for (int j = 0; j < alphabet.length(); j++) {
+					if (lowerInput.charAt(i) == alphabet.charAt(j)){
+						if(input.charAt(i) == Character.toUpperCase(alphabet.charAt(j)))
+							outputsb.append(Character.toUpperCase(Cipher1.charAt(j)));
+						else
+							outputsb.append(Cipher1.charAt(j));
+					}
+				}
+			}
+			else
+			{
+				for (int j = 0; j < alphabet.length(); j++) {
+					if (lowerInput.charAt(i) == alphabet.charAt(j)){
+						if(input.charAt(i) == Character.toUpperCase(alphabet.charAt(j)))
+							outputsb.append(Character.toUpperCase(Cipher2.charAt(j)));
+						else
+							outputsb.append(Cipher2.charAt(j));
+					}
+				}
+			}
+		}
+		return outputsb.toString();
+	}
 	private static void testCaesar(){
 		FileResource fr = new FileResource();
 		String message = fr.asString();
-		int key = 17;
-		int key2 = 23;
+		int key = 15;
+		//int key2 = 23;
 		String encrypted = encrypt(message, key);
 		System.out.println("key is " + key + "\n" + "Secret message is: " + encrypted);
-		encrypted = encrypt(message, key2);
-		System.out.println("key is " + key2 + "\n" + "Secret message is: " + encrypted);
+		//encrypted = encrypt(message, key2);
+		//System.out.println("key is " + key2 + "\n" + "Secret message is: " + encrypted);
 	}
-	
+	private static void testEncryptTwoKeys(){
+		FileResource fr = new FileResource();
+		String message = fr.asString();
+		int key1 = 8;
+		int key2 = 21;
+		String encrypted = encryptTwoKeys(message, key1, key2);
+		System.out.println("Secret message is: " + encrypted);
+	}
 	public static void main(String[] args){
-		testCaesar();
+		//testCaesar();
+		testEncryptTwoKeys();
 	}
 }
