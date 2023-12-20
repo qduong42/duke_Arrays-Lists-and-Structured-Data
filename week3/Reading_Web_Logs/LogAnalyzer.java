5b3eedf5482a9b81bc173b445c019df8c3d8d23f
acer42 <qduong@students.42wolfsburg.de>
2023-12-20 13:42:36 +0100

    includes not working well

---
+++ week3/Reading_Web_Logs/LogAnalyzer.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+import duke.edu.*;
+
+public class LogAnalyzer
+{
+     private ArrayList<LogEntry> records;
+     
+     public LogAnalyzer() {
+         records = new ArrayList <LogEntry>();
+     }
+        
+     public void readFile(String filename) {
+        FileResource fr = new FileResource(filename);
+        for(String line : fr.lines()){
+            LogEntry le = WebLogParser.parseEntry(line);
+            records.add(le);
+        }
+    }
+        
+     public void printAll() {
+         for (LogEntry le : records) {
+             System.out.println(le);
+         }
+     }
+     
+     
+}
